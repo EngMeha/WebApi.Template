8b39811e6ca7b1b72fae57061b189db163162fb2
Meha_meh <57707748+EngMeha@users.noreply.github.com>
2026-05-19 18:26:44 +0800

    refactor: update IUnitOfWork

--- WebApi.Template.Infrastructure/Data/EfUnitOfWork.cs
+++ WebApi.Template.Infrastructure/Data/EfUnitOfWork.cs
@@ -4,7 +4,7 @@ namespace WebApi.Template.Infrastructure.Data;
 
 public class EfUnitOfWork: IUnitOfWork
 {
-    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         //TODO прописать реализацию
         throw new NotImplementedException();
